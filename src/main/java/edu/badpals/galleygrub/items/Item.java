c454378796cd8414e600657c9c30a7f54b55c535
bestiadivina147 <diegopc147@gmail.com>
2024-04-15 10:07:26 +0200

    feat(Item.java): add all missing methods to Item.java

--- src/main/java/edu/badpals/galleygrub/items/Item.java
+++ src/main/java/edu/badpals/galleygrub/items/Item.java
@@ -1,32 +1,53 @@
 package edu.badpals.galleygrub.items;
 
-public class Item implements Product{
-    String name;
-    Double price;
-    String extra;
+public class Item implements Product {
+    String name="";
+    Double price=0d;
+    String extra="";
+
+    public Item(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Item(String name, Double price, String extra) {
+        this.name = name + " w/" + extra ;
+        this.price = price;
+        this.extra = extra;
+    }
 
     @Override
     public String name() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'name'");
+        return this.name;
     }
 
     @Override
     public Double price() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'price'");
+        return this.price;
     }
 
     @Override
     public String extra() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'extra'");
+        return this.extra;
     }
 
     @Override
     public Boolean isRegular() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isRegular'");
+        return this.extra().isBlank();
     }
-    
+    @Override
+    public String toString(){
+        return this.name + "...." + String.format("%.2f", this.price) +"$" + "\t";
+    }
+    public boolean equals(Object obj){
+        if(obj instanceof Item){
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public int hashCode(){
+        return this.name.hashCode();
+    }
+
 }

1ff6504ae17e219a2b5ca415987279922f4b3f1a
bestiadivina147 <diegopc147@gmail.com>
2024-04-15 09:05:03 +0200

    feat(Item.java): add Item.java

---
+++ src/main/java/edu/badpals/galleygrub/items/Item.java
@@ -0,0 +1,32 @@
+package edu.badpals.galleygrub.items;
+
+public class Item implements Product{
+    String name;
+    Double price;
+    String extra;
+
+    @Override
+    public String name() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'name'");
+    }
+
+    @Override
+    public Double price() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'price'");
+    }
+
+    @Override
+    public String extra() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'extra'");
+    }
+
+    @Override
+    public Boolean isRegular() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isRegular'");
+    }
+    
+}

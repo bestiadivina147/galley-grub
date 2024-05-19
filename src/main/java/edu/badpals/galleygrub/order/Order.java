375d271a5503e04fb3027726afe231f5d36cee5a
bestiadivina147 <diegopc147@gmail.com>
2024-05-19 18:08:13 +0200

    feat(Order.java): add methods to teh class Order.java

--- src/main/java/edu/badpals/galleygrub/order/Order.java
+++ src/main/java/edu/badpals/galleygrub/order/Order.java
@@ -2,8 +2,10 @@ package edu.badpals.galleygrub.order;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 import edu.badpals.galleygrub.items.Item;
+import edu.badpals.galleygrub.items.ItemFactory;
 
 public class Order implements Comanda {
     private Double total ;
@@ -13,21 +15,24 @@ public class Order implements Comanda {
 
     @Override
     public void addItem(String name, double price) {
+        Item item = ItemFactory.getItem(name, price);
+        this.items.add(item);
     }
 
     @Override
     public void addItem(String name, double price, String extra) {
+        Item item = ItemFactory.getItem(name, price, extra);
+        this.items.add(item);
     }
 
     @Override
     public int size() {
-        return  items.size();
+        return  this.items.size();
     }
 
     @Override
     public List<Item> itemList() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'itemList'");
+        return Collections.unmodifiableList(this.items);
     }
 
     @Override
@@ -38,10 +43,17 @@ public class Order implements Comanda {
 
     @Override
     public void updateTotal(Double price) {
+        this.total= total + total;
     }
 
+    private void itemDisplay(Item item) {
+        System.out.print("\t" + item.toString() + "\n");
+    } 
+    
     @Override
     public void display() {
+        System.out.print("\n\t --- ORDER --- \n");
+        itemList().stream().forEach(this::itemDisplay);
     }
 
 }

4f15acff33fbb51e016c5fe91b4861d3b6208677
bestiadivina147 <diegopc147@gmail.com>
2024-05-19 18:42:50 +0200

    build(CheeseExtra.java Extras.java): add cass Extras.java and CheeseExtra.java

---
+++ src/main/java/edu/badpals/galleygrub/extras/CheeseExtra.java
@@ -0,0 +1,22 @@
+package edu.badpals.galleygrub.extras;
+
+import edu.badpals.galleygrub.order.Comanda;
+import java.util.Optional;
+
+public class CheeseExtra extends Extra {
+
+    public CheeseExtra() {};
+
+    @Override
+    public void sumExtras(Comanda order) {
+        
+        Optional<Double> cheeseCharge = order.itemList().stream().filter(item -> item.extra().equalsIgnoreCase(Extras.CHEESE.name()))
+                            .map(item -> Extras.CHEESE.getPrice()).reduce(Double::sum);
+        if (cheeseCharge.isPresent()) {
+            order.updateTotal(cheeseCharge.get());
+        }
+        this.nextExtra.ifPresent(chain -> chain.sumExtras(order));
+
+    }
+    
+}
